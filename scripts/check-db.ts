aeafa39a7c53ad0fd5b329e993bf6dcdcd4a9523
jvelit89-ship-it <jvelit89@gmail.com>
2026-03-12 17:10:28 -0500

    Refactor check-db.ts to use async/await
    Updated database queries to use async/await for better handling of asynchronous operations.

--- scripts/check-db.ts
+++ scripts/check-db.ts
@@ -1,19 +1,18 @@
-
-import { db } from "./src/db";
-import { users, tenants, plans } from "./src/db/schema";
+import { db } from "../src/db";
+import { users, tenants, plans } from "../src/db/schema";
 
 async function check() {
-  console.log("--- Plans ---");
-  const allPlans = db.select().from(plans).all();
-  console.log(allPlans);
+    console.log("--- Plans ---");
+    const allPlans = await db.select().from(plans);
+    console.log(allPlans);
 
   console.log("--- Tenants ---");
-  const allTenants = db.select().from(tenants).all();
-  console.log(allTenants);
+    const allTenants = await db.select().from(tenants);
+    console.log(allTenants);
 
   console.log("--- Users ---");
-  const allUsers = db.select().from(users).all();
-  console.log(allUsers);
+    const allUsers = await db.select().from(users);
+    console.log(allUsers);
 }
 
 check().catch(console.error);
